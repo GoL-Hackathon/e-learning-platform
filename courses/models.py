608998d05607c4f42fd6c76fa79596bb2de76c86
weshy <josephwaweru96@gmail.com>
2022-08-01 13:13:18 +0300

    sample data and content model

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
 
 # Create your models here.
 class Subject(models.Model):
@@ -41,3 +44,13 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                                related_name='contents',
+                                on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, 
+                                        on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
