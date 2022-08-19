cd66d680076b12b19c5d4781970e6c469dfbe5dd
weshy <josephwaweru96@gmail.com>
2022-08-19 16:57:08 +0300

    permissions & serializing course contents

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 
-from ..models import Module, Subject, Course
+from ..models import Module, Subject, Course, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
-        fields = ('id', 'title', 'slug')
+        fields = ['id', 'title', 'slug']
 
 
 class ModuleSerialaizer(serializers.ModelSerializer):
@@ -21,3 +21,31 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview',
                         'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerialazer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerialazer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title',  'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'subject', 'slug', 'overview', 'created', 'owener', 'modules']
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import action
 
 from ..models import Course, Subject
-from .serializers import CourseSerializer, SubjectSerializer
+from .permissions import IsEnrolled
+from .serializers import (CourseSerializer, CourseWithContentsSerializer,
+                          SubjectSerializer)
 
 
 class SubjectListView(generics.ListAPIView):
@@ -41,3 +43,12 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.stuents.add(request.user)
         return Response({'enrolled': True})
+
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication,],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
