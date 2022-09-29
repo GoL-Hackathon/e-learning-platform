2e10c5f5a19536082ad2a2676a1a888e268f66bd
weshy <josephwaweru96@gmail.com>
2022-09-30 02:15:25 +0300

    removed most the unnecessary code

--- education/settings.py
+++ education/settings.py
@@ -53,18 +53,11 @@ INSTALLED_APPS = [
     'fontawesome',
 
 ]
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.jangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
